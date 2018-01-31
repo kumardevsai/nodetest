1e1a17965b1deeaad81ee4008d6e06824ed3f4f7
Vijay Kumar Papasani (Trianz) <VijayKumar.Papasani@trianz.com>
2018-01-31 15:22:40 +0530

    initail

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const normalizePort = require('normalize-port')
+
+//const port = process.env.PORT || 3000
+var port = normalizePort(process.env.PORT || process.env.VCAP_APP_PORT || '3000');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Test Node\n');
+}).listen(port, '0.0.0.0');
+console.log('Serverning at http://0.0.0.0:' + port.toString() + '/');
