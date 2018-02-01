35d2fb9ce2cdb95e17207830cb61a38d9b644cd3
Vijay Kumar Papasani (Trianz) <VijayKumar.Papasani@trianz.com>
2018-02-01 11:57:48 +0530

    new update

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ var port = normalizePort(process.env.PORT || process.env.VCAP_APP_PORT || '3000'
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hellow World2222222');
+  res.end('Hellow World3333333');
 }).listen(port, '0.0.0.0');
 console.log('Serverning at http://0.0.0.0:' + port.toString() + '/');
