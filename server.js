e80186b18b3bac4bd7a036e63b644c1ed6841383
Vijay Kumar Papasani (Trianz) <VijayKumar.Papasani@trianz.com>
2018-02-06 13:12:11 +0530

    issue

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.get('/con', function (req, res) {
     var url = getURI();
     mc.connect(url, function(err,db) {
 	assert.equal(null,err);
-	res.send("connected to server1");
+	res.send("connected to serverqq");
 	console.log("connected to server");
 	db.close()
     })
