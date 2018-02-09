8e81bebc6cf1e57bac4a3fefdc907e31b6c7649b
Vijay Kumar Papasani (Trianz) <VijayKumar.Papasani@trianz.com>
2018-02-09 16:16:01 +0530

    new changes

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.get('/con', function (req, res) {
     var url = getURI();
     mc.connect(url, function(err,db) {
 	assert.equal(null,err);
-	res.send("connected to serversfdsfdss sdfds dsfds");
+	res.send("connected to servers111");
 	console.log("connected to server");
 	db.close()
     })
